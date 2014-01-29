cdc21bb074f4a3eb0c18b68265d74d5b65a3b929
Whit Schrader <whit@schraderlabs.com>
2014-01-28 16:07:58 -0800

    initial commit

---
+++ apt_app.rb
@@ -0,0 +1,58 @@
+# building a rental apartment app
+
+#list of vacant units
+
+# is unit vacant
+
+# is unit occupied
+
+# Tenant in unit
+
+# tenant paid rent
+
+# move tenant in unit
+
+# remove
+
+# lease Rent
+
+# annual income
+
+
+
+class Building
+     @units
+     @address
+     @name
+
+	def calc_annual_income
+	end
+	
+end
+
+class Unit
+	@number
+	@rent
+	@sqft
+
+
+	def is_vacant
+	end
+
+	def method2
+	end
+
+class Tenant
+	@name
+	@phone
+	@unit
+	@lease
+
+	def method
+	end
+	def method
+	end
+
+end
+
+
